4317bfd6586d46158179c6da85355088f23746d3
rezwan2230 <rezwan2230@gmail.com>
2023-08-15 22:58:12 +0600

    approximat final except string handling

--- js/withdraw.js
+++ js/withdraw.js
@@ -2,5 +2,30 @@
 
 document.getElementById('withdrow-btn').addEventListener('click',function(){
     const withdrawInputField = document.getElementById('withdraw-input')
+    const withdrawAmountString = withdrawInputField.value
+    const CurrentwithdrawAmount = parseFloat(withdrawAmountString)
+
+
+    const withdrawTotalElement = document.getElementById('withdraw-balance')
+    const withdrawTotalElementString = withdrawTotalElement.innerText
+    const previousWithdrawAmount = parseFloat(withdrawTotalElementString)
+
+    const TotalWithdrawAmount = previousWithdrawAmount + CurrentwithdrawAmount
+
+
+    const totalBalanceElement = document.getElementById('total-balance')
+    const previoustotalBalanceString = totalBalanceElement.innerText
+    const previousTotalBalance = parseFloat(previoustotalBalanceString)
+
+    //step-6: set the balance total
+    if(CurrentwithdrawAmount > previousTotalBalance){
+        alert("Insufficient Balance")
+    }
+    else{
+        withdrawTotalElement.innerText = TotalWithdrawAmount
+        totalBalanceElement.innerText = previousTotalBalance - CurrentwithdrawAmount
+    }
     
+
+    withdrawInputField.value = ''
 })
