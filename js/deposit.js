ed57e0d977aacb1eeb72dd5121000d2f88bb289d
rezwan2230 <rezwan2230@gmail.com>
2023-08-18 15:19:11 +0600

    added user input validation

--- js/deposit.js
+++ js/deposit.js
@@ -1,6 +1,12 @@
 
 document.getElementById('deposit-btn').addEventListener('click',function(){
     const newDepositAmount = getInputFieldValueById('deposite-input')
+
+    if(isNaN(newDepositAmount)){
+        alert("Please enter a number.")
+        return
+    }
+
     const previousDepositAmount = getInputValueById('diposit-balance')
     const currentDepositAmount = previousDepositAmount + newDepositAmount
 
--- js/withdraw.js
+++ js/withdraw.js
@@ -2,6 +2,11 @@
 document.getElementById('withdrow-btn').addEventListener('click', function () {
 
     const newwithdrowAmount = getInputFieldValueById('withdraw-input')
+
+    if(isNaN(newwithdrowAmount)){
+        alert("Please enter a number.")
+        return
+    }
     const previouswithdrowAmount = getInputValueById('withdraw-balance')
     const currentwithdrowAmount = previouswithdrowAmount + newwithdrowAmount
 
